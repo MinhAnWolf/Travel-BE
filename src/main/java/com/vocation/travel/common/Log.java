de75c0e550ec5ac3029114290e83fc4aff37da73
Minh An <minhanlefz09@gmail.com>
2024-04-07 21:37:52 +0700

    #16 | [API] Address

---
+++ src/main/java/com/vocation/travel/common/Log.java
@@ -0,0 +1,44 @@
+package com.vocation.travel.common;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Log {
+    public static void startLog(String method) {
+        log.info("METHOD: {}", method);
+    }
+
+    public static void startLog(String service, String method) {
+        log.info("START SERVICE: {}", service);
+        log.info("START METHOD: {}", method);
+    }
+
+    public static void inputLog(Object... data) {
+        log.info("INPUT: {}", data);
+    }
+
+    public static void debugLog(Object... data) {
+        log.debug("DATA: {}", data);
+    }
+
+    public static void outputLog(Object data) {
+        log.debug(  "Result: {}", data);
+    }
+
+    public static void endLog(String service, String method) {
+        log.info("END METHOD: {}", method);
+        log.info("END SERVICE: {}", service);
+    }
+
+    public static void errorLog(String message, Exception exception) {
+        log.error("ERROR: {}", message, exception);
+    }
+
+    public static void errorLog(Exception exception) {
+        log.error("ERROR: ", exception);
+    }
+
+    public static void errorLog(String message) {
+        log.error("ERROR: {}", message);
+    }
+}
